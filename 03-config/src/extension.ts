8a6f46096eb4c744b11b03b9b4a70881bba347d8
kjfsm <glaygraze@gmail.com>
2020-08-30 14:31:26 +0900

    03

--- 03-config/src/extension.ts
+++ 03-config/src/extension.ts
@@ -1,11 +1,30 @@
 import * as vscode from "vscode";
 
-export function activate(context: vscode.ExtensionContext) {
+export const activate = (context: vscode.ExtensionContext) => {
   context.subscriptions.push(
-    vscode.commands.registerCommand("03-config.loadConfig", () => {
-      vscode.window.showInformationMessage("Hello World from 03-config!");
+    vscode.commands.registerCommand("03-config.loadConfig", async () => {
+      const configuration = vscode.workspace.getConfiguration("config");
+
+      const get = (section: string): string => {
+        const value = configuration.get(section);
+        if (value === undefined) {
+          return "undefined";
+        }
+        switch (typeof value) {
+          case "string":
+            return "string " + value;
+          case "boolean":
+            return "boolean " + value;
+          default:
+            return "unknown";
+        }
+      };
+
+      vscode.window.showInformationMessage(get("string"));
+      vscode.window.showInformationMessage(get("boolean"));
+      vscode.window.showInformationMessage(get("object"));
     })
   );
-}
+};
 
 export function deactivate() {}
